df5b6b68afbe5c8cce9d8f69afdba6bafd4bc7f7
sanjaysanghera <sanjay.sanghera@gmail.com>
2021-04-02 17:21:28 +0000

    Created email function for contact form.

---
+++ static/js/email.js
@@ -0,0 +1,19 @@
+/* Function for the contact form to send an email */
+
+function sendMail(contactForm) {
+    emailjs.send("service_7ead7y8", "discover_sydney", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "discover_sydney_feedback": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            document.getElementById("myForm").reset();
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
