14cc25126281eb8ec0760ba28e9eb05d7bcd13a6
sanjaysanghera <sanjay.sanghera@gmail.com>
2021-04-20 11:07:01 +0000

    Beautified code.

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,6 @@
-var map;
+let map;
 
-var infoWindow;
+let infoWindow;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
@@ -15,19 +15,19 @@ function initMap() {
 
 /* Active markers variable */
 
-var activeMarkers = [];
+let activeMarkers = [];
 
 /* Active markers function */
 
 function addMarker(property) {
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
         position: property.location,
         map: map,
     });
 
     activeMarkers.push(marker);
 
-    marker.addListener("click", function () {
+    marker.addListener("click", function() {
         infoWindow.close();
         infoWindow.setContent(property.content);
         infoWindow.open(map, marker);
@@ -44,32 +44,27 @@ function clearMarkers() {
 
 /* Explore buttons function */
 
-$(document).ready(function () {
-    $("#attractions").on("click", function () {
+$(document).ready(function() {
+    $("#attractions").on("click", function() {
         clearMarkers();
         showMarkers(attractionsMarkers);
     });
-});
 
-$(document).ready(function () {
-    $("#barsclubs").on("click", function () {
+    $("#barsclubs").on("click", function() {
         clearMarkers();
         showMarkers(barsclubsMarkers);
     });
-});
 
-$(document).ready(function () {
-    $("#hotels").on("click", function () {
+    $("#hotels").on("click", function() {
         clearMarkers();
         showMarkers(hotelsMarkers);
     });
-});
 
-$(document).ready(function () {
-    $("#restaurants").on("click", function () {
+    $("#restaurants").on("click", function() {
         clearMarkers();
         showMarkers(restaurantsMarkers);
     });
+
 });
 
 /* Show markers loop */
@@ -78,67 +73,102 @@ function showMarkers(markers) {
     for (i = 0; i < markers.length; i++) {
         addMarker(markers[i]);
     }
-    map.setCenter({ lat: -33.86882, lng: 151.209296 });
+    map.setCenter({
+        lat: -33.86882,
+        lng: 151.209296
+    });
 }
 
 /* Map markers */
 
-var attractionsMarkers = [
-    {
-        location: { lat: -33.856543821752794, lng: 151.21527524045297 },
+let attractionsMarkers = [{
+        location: {
+            lat: -33.856543821752794,
+            lng: 151.21527524045297
+        },
         content: "<h6>Sydney Opera House</h6>"
     },
     {
-        location: { lat: -33.8520746192094, lng: 151.2107870981232 },
+        location: {
+            lat: -33.8520746192094,
+            lng: 151.2107870981232
+        },
         content: "<h6>Sydney Harbour Bridge</h6>"
     },
     {
-        location: { lat: -33.87084698630033, lng: 151.20909379812386 },
+        location: {
+            lat: -33.87084698630033,
+            lng: 151.20909379812386
+        },
         content: "<h6>Sydney Tower Eye</h6>"
     }
 ];
 
-var barsclubsMarkers = [
-    {
-        location: { lat: -33.87003959244973, lng: 151.2051437269588 },
+let barsclubsMarkers = [{
+        location: {
+            lat: -33.87003959244973,
+            lng: 151.2051437269588
+        },
         content: "<h6>Catina OK</h6>"
     },
     {
-        location: { lat: -33.86832748834104, lng: 151.2048179134643 },
+        location: {
+            lat: -33.86832748834104,
+            lng: 151.2048179134643
+        },
         content: "<h6>PS40</h6>"
     },
     {
-        location: { lat: -33.86710113454278, lng: 151.19558052695868 },
+        location: {
+            lat: -33.86710113454278,
+            lng: 151.19558052695868
+        },
         content: "<h6>Marquee Sydney</h6>"
     }
 ];
 
-var hotelsMarkers = [
-    {
-        location: { lat: -33.868516113790704, lng: 151.22040862695886 },
+let hotelsMarkers = [{
+        location: {
+            lat: -33.868516113790704,
+            lng: 151.22040862695886
+        },
         content: "<h6>Ovolo Woolloomooloo</h6>"
     },
     {
-        location: { lat: -33.86532426380766, lng: 151.2051743711346 },
+        location: {
+            lat: -33.86532426380766,
+            lng: 151.2051743711346
+        },
         content: "<h6>Little National Hotel Sydney</h6>"
     },
     {
-        location: { lat: -33.872457090332645, lng: 151.19710524229967 },
+        location: {
+            lat: -33.872457090332645,
+            lng: 151.19710524229967
+        },
         content: "<h6>Ovolo 1888 Darling Harbour</h6>"
     }
 ];
 
-var restaurantsMarkers = [
-    {
-        location: { lat: -33.85722877121859, lng: 151.21035336928813 },
+let restaurantsMarkers = [{
+        location: {
+            lat: -33.85722877121859,
+            lng: 151.21035336928813
+        },
         content: "<h6>Quay</h6>"
     },
     {
-        location: { lat: -33.886254831374224, lng: 151.22991487113518 },
+        location: {
+            lat: -33.886254831374224,
+            lng: 151.22991487113518
+        },
         content: "<h6>Saint Peter</h6>"
     },
     {
-        location: { lat: -33.88908454294458, lng: 151.1654055435762 },
+        location: {
+            lat: -33.88908454294458,
+            lng: 151.1654055435762
+        },
         content: "<h6>Sixpenny</h6>"
     }
 ];
